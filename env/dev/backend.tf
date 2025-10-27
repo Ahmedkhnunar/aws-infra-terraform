a7bdf12b904641912fba2f3f2c27cadf4fdcb72a
Ahmed <ahmedkhanunar@gmail.com>
2025-10-27 13:12:09 +0500

    Create backend.tf

---
+++ env/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "123-porter-dev-ops-terraform"       # S3 bucket name
+    key            = "envs/dev/terraform.tfstate" # Path within the bucket
+    region         = "us-east-1"                 # Bucket region
+    encrypt        = true                        # Encrypt state at rest
+    # dynamodb_table = "terraform-locks"           # Optional: for state locking
+  }
+}
