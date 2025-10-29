f2c30d371de93729759b4298b4fb14e12b020950
Ahmed <ahmedkhanunar@gmail.com>
2025-10-29 15:53:35 +0500

    Update outputs.tf

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -1,5 +1,14 @@
-// Outputs for module: subnet
-output "id" {
-  value       = ""
-  description = "Resource ID"
+output "subnet_ids" {
+  description = "Map of subnet IDs created by this module"
+  value       = { for k, subnet in aws_subnet.managed : k => subnet.id }
+}
+
+output "subnet_arns" {
+  description = "Map of subnet ARNs created by this module"
+  value       = { for k, subnet in aws_subnet.managed : k => subnet.arn }
+}
+
+output "subnet_cidr_blocks" {
+  description = "Map of subnet CIDR blocks"
+  value       = { for k, subnet in aws_subnet.managed : k => subnet.cidr_block }
 }
